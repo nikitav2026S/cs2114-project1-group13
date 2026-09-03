61d6f2a2a7365806b1d0100908e3107ee985561e
ayaand30 <ayaand30@vt.edu>
2026-09-03 15:40:33 -0400

    Add additional print statement to main method

--- mehulmushuni.java
+++ mehulmushuni.java
@@ -1,5 +1,6 @@
 public class Mehulmushuni{
     public static void main(String[] args) {
         System.out.println("Mehul Mushuni is on the team.");
+        System.out.println("lowke he is tho");
     }
 }
